f0dbb08761942432672d01857338b574be9bc37a
AlexandrZiminov <samazima@mail.ru>
2024-04-26 19:56:37 +0400

    Fixed bug

--- Program.cs
+++ Program.cs
@@ -8,14 +8,14 @@ internal class Program
         var randInt = new Random().Next(0, (int)maxValue);
         var attempts = (int)Math.Ceiling(Math.Log2(maxValue));
         var currentGuess = Math.Ceiling(maxValue / 2);
-        maxValue = Math.Ceiling(maxValue / 2);
+        var step = Math.Ceiling(maxValue / 2);
 
         Console.WriteLine(
             $"I have an integer number from 0 to {maxValue}, you have {attempts} attempts, try to guess it");
 
         for (var i = 0; i < attempts; i++)
         {
-            maxValue = Math.Ceiling(maxValue / 2);
+            step = Math.Ceiling(step / 2);
             Console.WriteLine($"Is it {currentGuess}?");
             var res = randInt > currentGuess ? "greater" : "less";
             Console.WriteLine(currentGuess == randInt
@@ -28,10 +28,10 @@ internal class Program
             }
 
             if (res == "greater")
-                currentGuess = currentGuess + maxValue;
+                currentGuess = currentGuess + step;
 
             else if (res == "less")
-                currentGuess = currentGuess - maxValue;
+                currentGuess = currentGuess - step;
         }
     }
 }
